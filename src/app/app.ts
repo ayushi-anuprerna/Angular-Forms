7331d02605beda867871aef35df26be2a9c0b3ba
ayushi-anuprerna <singhaayu3003@gmail.com>
2026-04-05 00:26:58 +0530

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { Level1Component } from './Level1Component/level-1';
 import { Level2Component } from './Level2Component/level-2';
+import { Level3 } from './Level3Component/level-3';
 import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule,Level2Component],
+  imports: [RouterOutlet,CommonModule,Level2Component,Level3],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

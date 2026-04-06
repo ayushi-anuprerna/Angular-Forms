351bec9ffafb536bb18e987b48e9940239d55f1f
ayushi-anuprerna <singhaayu3003@gmail.com>
2026-04-06 16:38:30 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { Level1Component } from './Level1Component/level-1';
 import { Level2Component } from './Level2Component/level-2';
-import { Level3 } from './Level3Component/level-3';
+import { Level3 } from './level-3/level-3';
 export const routes: Routes = [
-    {path:"level-1" ,component:Level1Component},
-    {path:"" ,component:Level2Component}  
-    {path:"level-3" ,component:Level3}
+    {path:'' ,component:Level1Component},
+    {path:'level-2' ,component:Level2Component},
+    {path:'level-3' ,component:Level3}
 ];

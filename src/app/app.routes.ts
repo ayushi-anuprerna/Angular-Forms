d4d5716ba08339917f9929b40fe14618c4c5f61a
ayushi-anuprerna <singhaayu3003@gmail.com>
2026-04-06 16:39:42 +0530

    Added level wise assignment view buttons

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { Level1Component } from './Level1Component/level-1';
 import { Level2Component } from './Level2Component/level-2';
-import { Level3 } from './level-3/level-3';
+import { Level3Component } from './level-3/level-3';
 export const routes: Routes = [
     {path:'' ,component:Level1Component},
     {path:'level-2' ,component:Level2Component},
-    {path:'level-3' ,component:Level3}
+    {path:'level-3' ,component:Level3Component}
 ];
